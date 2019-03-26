6cabfbcbc1f55b3925b46e627f0214fad178bc56
PolarisJunior <brownasianpj@yahoo.com>
2019-03-25 17:45:29 -0700

    example text to speech

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 const YouTube = require("youtube-node");
 const auth = require("./auth.json");
+const fs = require('fs');
+const aws = require("aws-sdk");
+const StreamBuffers = require("stream-buffers");
+
 const client = new Discord.Client;
 
 const youTube = new YouTube();
@@ -11,6 +15,20 @@ youTube.setKey(auth.yt_token);
 
 let commands = ["play", "help", "queue", "skip", "pick"];
 let numberEmotes = [":one:", ":two:", ":three:", ":four:", ":five:"];
+
+const polly = new aws.Polly({
+    signatureVersion: 'v4',
+    region: 'us-west-1'
+});
+
+let params = {
+    'Text': 'I am monky a a a',
+    'OutputFormat': 'mp3',
+    // 'OutputFormat': 'pcm',
+    'VoiceId': 'Joey'
+};
+
+
 
 client.on("ready", () => {
     console.log("Monky ready");
@@ -26,12 +44,61 @@ let playlist = [];
 // current youtube search results
 let currentSearch = null;
 
+function joinAndQueueUp() {
+
+}
 
 client.on("message", (message) => {
     if (message.content.startsWith("!")) {
         let args = message.content.substring(1).split(" ");
-        
+
         switch (args[0].toLowerCase()) {
+            case "join": {
+                let voiceChannel = message.member.voiceChannel;
+                if (!voiceChannel || voiceChannel.type != "voice") {
+                    message.reply("monky need voice channel");
+                } else {
+                    voiceChannel.join().then(connection => {
+
+                        polly.synthesizeSpeech(params, (err, data) => {
+                            if (err) {
+                                console.log(err.code)
+                            } else if (data) {
+                                if (data.AudioStream instanceof Buffer) {
+                                    fs.writeFile("./speech.mp3", data.AudioStream, function (err) {
+                                        if (err) {
+                                            return console.log(err)
+                                        }
+                                        console.log("The file was saved!")
+                                        let dispatcher = connection.playFile("./speech.mp3");
+                                        dispatcher.on("end", reason => {
+                                            // console.log(reason);
+                                            voiceChannel.leave();
+                                        });
+                                    });
+                                }
+                            }
+                        });
+
+
+                        // const receiver = connection.createReceiver();
+
+                        // connection.on("speaking", (user, speaking) => {
+                        //     const audioStream = receiver.createOpusStream(user);
+                        //     if (speaking) {
+                        //         msg.channel.send(`i'm listening to ${user}`);
+                        //         audioStream.on("data", (chunk) => {
+                        //             console.log("got data");
+                        //         });
+                        //         audioStream.on("end", () => {
+                        //             message.channel.send(`Monkey done listening`);
+                        //         });
+                        //     }
+                        // });
+                    });
+                }
+            }
+                break;
             case "play": {
                 let voiceChannel = message.member.voiceChannel;
                 if (!voiceChannel) {
@@ -87,7 +154,7 @@ client.on("message", (message) => {
                 message.channel.send("monky will play " + currentSearch.items[value].snippet.title);
 
                 let voiceChannel = message.member.voiceChannel;
-                
+
 
                 if (voiceChannel) {
                     voiceChannel.join().then(
