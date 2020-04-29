9eef828ff8d59f85c5cb4647797be73c5afd4c95
PolarisJunior <brownasianpj@yahoo.com>
2020-04-29 00:12:03 -0400

    Separate search and play

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const youTube = new YouTube();
 const ytSearch = util.promisify(youTube.search);
 youTube.setKey(config.youtube_token);
 
-const commands = ["play", "help", "queue", "skip", "pick", "stop"];
+const commands = ["play", "search", "help", "queue", "skip", "pick", "stop"];
 const numberEmotes = [":one:", ":two:", ":three:", ":four:", ":five:"];
 
 const polly = new aws.Polly({
@@ -32,25 +32,28 @@ let currentSearch = null;
 
 function playSongs(connection, voiceChannel) {
   if (playlist.length > 0) {
-    let songUrl = playlist[0];
+    const songUrl = playlist[0];
     const stream = ytdl(songUrl, {
       filter: "audioonly",
       quality: "highestaudio",
     });
     let dispatcher = connection.play(stream);
 
-    // dispatcher.on("end", (reason) => {
-    //     console.log("Finished because: " + reason);
-    //     playlist.shift();
-    //     stream.destroy();
-    //     playSongs(connection, voiceChannel);
-    // });
+    dispatcher.on("speaking", (isSpeaking) => {
+      if (!isSpeaking) {
+        playlist.shift();
+        stream.destroy();
+        playSongs(connection, voiceChannel);
+      }
+    });
   } else {
     voiceChannel.leave();
   }
 }
 
-function ytPrintSearchResults(results, userMessage) {
+function doYtSearch(query) {}
+
+function ytDisplaySearchResults(results, userMessage) {
   let numResults = results.items.length;
   let msg = "";
   for (let i = 0; i < numResults; i++) {
@@ -58,10 +61,9 @@ function ytPrintSearchResults(results, userMessage) {
   }
   msg += "!pick # to choose";
   userMessage.channel.send(msg);
-  currentSearch = results;
 }
 
-function handleHelpRequest(userMessage) {
+function handleHelpRequest(userMessage, args) {
   let res = "monky see and monky do:\n";
   for (c in commands) {
     res += `\`!${commands[c]}\`\n`;
@@ -69,7 +71,7 @@ function handleHelpRequest(userMessage) {
   userMessage.reply(res);
 }
 
-function handleQueueRequest(userMessage) {
+function handleQueueRequest(userMessage, args) {
   if (playlist.length > 0) {
     let res = playlist.join("\n");
     userMessage.channel.send(res);
@@ -78,7 +80,69 @@ function handleQueueRequest(userMessage) {
   }
 }
 
-function handleStopRequest(userMessage) {
+function handlePlayRequest(userMessage, args) {
+  if (!ensureInVoiceChannel(userMessage)) {
+    return;
+  }
+  if (args.length < 2) {
+    userMessage.reply("monky need url or search term");
+    return;
+  }
+  if (args[1].includes("https://www.youtube.com/watch")) {
+    let url = args[1];
+    voiceChannel.join().then((connection) => {
+      playlist.push(url);
+      if (playlist.length == 1) {
+        playSongs(connection, voiceChannel);
+      }
+    });
+  } else {
+    const query = argsToSearchQuery(args);
+    ytSearch(query, 1)
+      .then((results) => {
+        setSearchContext(results);
+        pickSongFromSearch(1, userMessage);
+      })
+      .catch((reason) => {
+        console.log(`Could not play from youtube ${reason}`);
+      });
+  }
+}
+
+function handleSearchRequest(userMessage, args) {
+  if (!ensureInVoiceChannel(userMessage)) {
+    return;
+  }
+  if (args.length < 2) {
+    userMessage.reply("monky need search term");
+  } else {
+    const query = argsToSearchQuery(args);
+
+    ytSearch(query, 5)
+      .then((results) => {
+        ytDisplaySearchResults(results, userMessage);
+        setSearchContext(results);
+      })
+      .catch((reason) => {
+        console.log(`Could not search youtube ${reason}`);
+      });
+  }
+}
+
+function handlePickRequest(message, args) {
+  if (currentSearch == null || args.length > 2) {
+    return;
+  }
+  let value = parseInt(args[1]);
+  if (value == NaN) {
+    message.reply(`monky don't understand \`${args[1]}\``);
+    return;
+  }
+  value--;
+  pickSongFromSearch(value, message);
+}
+
+function handleStopRequest(userMessage, args) {
   playlist = [];
   const voiceChannel = userMessage.member.voice.channel;
 }
@@ -163,70 +227,21 @@ function routeCommand(message) {
       }
       break;
     case "play": {
-      let voiceChannel = message.member.voice.channel;
-      if (!voiceChannel) {
-        message.reply("monky need voice channel");
-      } else if (args.length < 2) {
-        message.reply("monky need url or search term");
-      } else {
-        if (args[1].includes("https://www.youtube.com/watch")) {
-          let url = args[1];
-          voiceChannel.join().then((connection) => {
-            playlist.push(url);
-            if (playlist.length == 1) {
-              playSongs(connection, voiceChannel);
-            }
-          });
-        } else {
-          args.splice(0, 1);
-          let query = args.join(" ");
-
-          ytSearch(query, 5)
-            .then((results) => ytPrintSearchResults(results, message))
-            .catch((reason) => {
-              console.log(`Could not search youtube ${reason}`);
-            });
-        }
-      }
+      handlePlayRequest(message, args);
       break;
     }
     case "pick": {
-      if (currentSearch == null || args.length > 2) {
-        return;
-      }
-      let value = parseInt(args[1]);
-      if (value == NaN) {
-        message.reply(`monky don't understand \`${args[1]}\``);
-        return;
-      }
-      value--;
-      if (value >= currentSearch.items.length) {
-        message.reply(`monky not have ${value} banan`);
-      }
-      let url = `https://www.youtube.com/watch?v=${currentSearch.items[value].id.videoId}`;
-      message.channel.send(
-        `monky will play \`${currentSearch.items[value].snippet.title}\``
-      );
-      message.channel.send(`${url}`);
-      let voiceChannel = message.member.voice.channel;
-
-      if (voiceChannel) {
-        voiceChannel.join().then((connection) => {
-          playlist.push(url);
-          // If playerlist is of size 1 then song was not previously playing.
-          if (playlist.length == 1) {
-            playSongs(connection, voiceChannel);
-          }
-        });
-      }
-      currentSearch = null;
+      handlePickRequest(message, args);
       break;
     }
+    case "search":
+      handleSearchRequest(message, args);
+      break;
     case "queue":
-      handleQueueRequest(message);
+      handleQueueRequest(message, args);
       break;
     case "help":
-      handleHelpRequest(message);
+      handleHelpRequest(message, args);
       break;
     default:
       message.reply(`monky don't understand \`${command}\``);
@@ -241,6 +256,54 @@ function parseMessageArgs(message) {
   let args = message.content.substring(1).split(" ");
   return args;
 }
+
+function setSearchContext(results) {
+  currentSearch = results;
+}
+
+function ensureInVoiceChannel(userMessage) {
+  const voiceChannel = userMessage.member.voice.channel;
+  if (!voiceChannel) {
+    userMessage.reply("monky need voice channel");
+    return false;
+  }
+  return true;
+}
+
+function argsToSearchQuery(args) {
+  args.splice(0, 1);
+  return args.join(" ");
+}
+
+function pickSongFromSearch(songNumber, message) {
+  if (songNumber > currentSearch.items.length) {
+    message.reply(`monky not have ${songNumber} banan`);
+    return;
+  }
+
+  // Go from 1-indexed to 0-indexed
+  songNumber--;
+
+  let url = `https://www.youtube.com/watch?v=${currentSearch.items[songNumber].id.videoId}`;
+  message.channel.send(
+    `monky will play \`${currentSearch.items[songNumber].snippet.title}\``
+  );
+  message.channel.send(`${url}`);
+  let voiceChannel = message.member.voice.channel;
+
+  if (voiceChannel) {
+    voiceChannel.join().then((connection) => {
+      playlist.push(url);
+      // If playlist is of size 1 then song was not previously playing.
+      if (playlist.length == 1) {
+        playSongs(connection, voiceChannel);
+      }
+    });
+  }
+  currentSearch = null;
+}
+
+function playNextInQueue() {}
 
 setCallbacks();
 console.log("Client logging on");
